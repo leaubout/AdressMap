85134bc75a3628545b73679f5a7be363b82dfb7a
P1 <leaubout@gmail.com>
2014-10-30 12:28:07 +0100

    marqueur et info bulle ok à partir d'une adresse en dur

--- src/public/testmap.php
+++ src/public/testmap.php
@@ -5,25 +5,99 @@
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
         <meta charset="utf-8">
         <style>
-            html, body, #map-canvas {
-	           height: 100%;
-	           margin: 0px;
-	           padding: 0px
+            html, body{
+              height: 100%;
+              margin: 0px;
+              padding: 0px
             }
+
+            #map-canvas{
+              width: 80%;
+              height: 600px;
+              margin: 50px auto;
+            }
+
+            h4{
+              margin-top, margin-bottom: 3px;
+            }
+
+            p{
+              margin-top, margin-bottom: 2px;
+            }
+
         </style>
     </head>
     <body>
+      <div>138 rue Marcadet 75018 Paris</div>
     	<div id="map-canvas"></div>
     	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
     	<script>
+
+          var geocoder;
+          var map;
+          var marker;
+
+          function codeAddress() {
+            var address = "138 rue Marcadet 75018 Paris";
+            geocoder.geocode( { 'address': address}, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location
+                });
+
+              var controlDiv = document.createElement('div');
+              var controlTitle = document.createElement('h4');
+              controlTitle.style.marginTop = '3px';
+              controlTitle.style.marginBottom = '3px';
+              controlTitle.innerHTML = "Ligue de défense des droits de l'homme";
+              controlDiv.appendChild(controlTitle);
+              
+              var controlDescription = document.createElement('p');
+              controlDescription.style.marginTop = '2px';
+              controlDescription.style.marginBottom = '2px';
+              controlDescription.innerHTML = "Association de défense des droits de l'homme";
+              controlDiv.appendChild(controlDescription);
+
+              var controlHr = document.createElement('hr');
+              controlDiv.appendChild(controlHr);
+
+              var controlUrl = document.createElement('a');
+              controlUrl.style.marginBottom = '5px';
+              controlUrl.setAttribute('href','http://www.ldh-france.org');
+              controlUrl.innerHTML = "www.ldh-france.org";
+              controlDiv.appendChild(controlUrl);
+
+              var infowindow = new google.maps.InfoWindow({
+                content: controlDiv});
+
+              infowindow.open(map,marker);
+              
+              google.maps.event.addListener(marker,'click', function(){
+                infowindow.open(map,marker)});
+
+              } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+              }
+            });
+            //console.log(marker);
+          }
+
            function initialize() {
+
               var mapOptions = {
                  zoom: 7,
                  center: new google.maps.LatLng(48.857, 2.352)
               };
-              var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+              map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+              console.log("avant code address");console.log(marker);
+              geocoder = new google.maps.Geocoder();
+              marker = codeAddress();
+              console.log("après code address");console.log(marker);
            }
-        
+
            google.maps.event.addDomListener(window, 'load', initialize);
         </script>
     </body>
